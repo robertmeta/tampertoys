e9032c3744f7f7b8e38612db58bdbdce76da1668
Robert Melton <robert@robertmelton.com>
2015-05-10 01:21:04 -0400

    Create reddit_link_name_highlighter.js

---
+++ reddit_link_name_highlighter.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Reddit Link Name Highlighter
+// @namespace    http://robertmeta.com
+// @version      0.1
+// @description  When you are linked to a comment, sometimes you want to see all the other comments by that person, this does that and nothing else
+// @author       Robert Melton
+// @match        http*://www.reddit.com/r/*/comments/*
+// @grant        none
+// ==/UserScript==
+var commentSection = document.getElementsByClassName('sitetable nestedlisting');
+var linksInCommentSection = commentSection[0].getElementsByTagName("a");
+var originalPersonLink = "";
+var color = "lightgreen";
+for ( i = 0; i < linksInCommentSection.length; i++ ) { 
+    if (linksInCommentSection[i].href.match("user")) { 
+        if (originalPersonLink === "") { // first person
+            originalPersonLink = linksInCommentSection[i].href;
+        }
+        if (linksInCommentSection[i].href.match(originalPersonLink)) {
+            linksInCommentSection[i].style.background = color;
+        }
+    }
+}
